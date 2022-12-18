86d40f5273d95ceaa6cf4f60ce91f7a5d0372c64
v_seo@list.ru <your_github_email>
2022-12-18 23:32:51 +0300

    inteerface, alias, sm-hm

--- block_2/src/lessons_1_interface.ts
+++ block_2/src/lessons_1_interface.ts
@@ -9,9 +9,17 @@ interface User {
 
 }
 
-interface Window {
-    isAuth?: boolean;
+interface Employee extends User, Person {
+    contractStart: Date,
 }
+
+// interface Developer extends Employee {
+//     skills: string[],
+//     phone: string,
+//     level? : 'junior' | 'middle' | 'senior'
+//     say(): void,
+//     code: (args:string) => void
+// }
 
 interface Person {
     readonly firstName: string;
@@ -20,25 +28,24 @@ interface Person {
     yearOfBirth?: number
 }
 
+export interface Window {
+    isAuth?: boolean;
+}
+
 const user1: User = {
     email: 'someUser@damain.com',
+    login : 'SomeLogin',
+    password: 'pass',
     isOnline: true,
-}
-
-interface Employee extends User, Person {
-    contractStart: Date,
-}
-
-interface Developer extends Employee {
-    skills: string[],
-    phone: string,
-    level? : 'junior' | 'middle' | 'senior'
-    say(): void,
-    code: (args:string) => void
 }
 
 const user2: Employee = {
     firstName: 'Modric',
     lastName: 'Vlasic',
-
+    email: 'someUser@domain.com',
+    login : 'oneLogin',
+    password: 'passom',
+    contractStart: new Date()
 }
+
+console.log(user1, user2)
--- block_2/src/lessons_2_alias.ts
+++ block_2/src/lessons_2_alias.ts
@@ -2,7 +2,36 @@ type Union1 = 'a'| 'b' | 'c' | 'd';
 type Union2 = 'a'| 'e' | 'c' | 'i';
 type Union3 = Union1 | Union2;
 type Union4 = Union1 & Union2;
+type Union5 = {a:string, b: string, c : number} & {a:string, t: boolean, z:null}
 
+
+const union3:Union5 = {a: 'Zenit' ,b: 'Kama' ,c: 4, t:true, z:null}
 const union1:Union3 = 'e' //all from Union1,2
 const union2:Union4 = 'c' //only twice from Union 1,2
+
+type User = {
+    isOnline?: boolean,
+    readonly email: string,
+    readonly login: string
+}
+
+type Person = {
+    readonly firstName: string;
+    lastName: string,
+    phone?: string,
+    yearOfBirth?: number
+}
+
+type Employee = {
+    contractStart: Date,
+} & Person & User
+
+type Developer = {
+    skills: string[],
+    phone: string,
+    level? : 'junior' | 'middle' | 'senior'
+    say(): void,
+    code: (args:string) => void
+} & Employee
+
 
